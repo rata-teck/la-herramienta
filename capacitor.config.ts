b451beab8acac5576ef9216189b92f6f53ee3650
matjimenez <mat.jimenezb@duocuc.cl>
2022-10-31 12:02:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'la herramienta',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
